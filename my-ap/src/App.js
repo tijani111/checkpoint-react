79387a554f73b407b4d96069da2e6eb82356f641
mohamed tijani zouari <mohamed_tijani.zouari@smail.th-koeln.de>
2021-09-23 03:01:35 +0200

    mon premier commit

---
+++ my-ap/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+
+import './component1.js';
+import SignUp from './component1.js';
+function App() {
+  return (
+    <div className="App">
+   <SignUp />
+  </div>
+  );
+}
+
+export default App;
